cf49963765995bde2c5bbd6fe3323e3acad55afa
Felipe Carvalho <64768561+FelipeGuglielmeli@users.noreply.github.com>
2024-06-25 17:37:02 -0300

    Ajustes finais

--- app/config.py
+++ app/config.py
@@ -12,5 +12,7 @@ print(f"DB_PORT: {os.environ.get('DB_PORT')}")
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASSWORD")}@{os.environ.get("DB_HOST")}:{os.environ.get("DB_PORT")}/{os.environ.get("DB_NAME")}'
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{os.environ.get("DB_USER")}:
+                                {os.environ.get("DB_PASSWORD")}@{os.environ.get("DB_HOST")}:
+                                {os.environ.get("DB_PORT")}/{os.environ.get("DB_NAME")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
